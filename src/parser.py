7ef4627daa966e27eb5f1b92f018ad6f37a31e7b
ilSyAbRi <iliasssyabri1@gmail.com>
2026-05-19 21:05:29 +0100

    first step in parsing

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,14 @@
-
+class PersonalError(Exception):
+    pass
 
 class parser:
-    pass
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_raw_input(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                content = file.readlines()
+                return content
+        except (PermissionError, FileNotFoundError, IsADirectoryError) as e:
+            raise PersonalError(f"file error: {e}")
