46d16f12987d784087c5a214930957e38b6276bc
ilSyAbRi <iliasssyabri1@gmail.com>
2026-05-20 21:33:36 +0100

    first step in main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
 import sys
-from parser import parser
+from parser import Parser, PersonalError
+
 
 DEFAULT_PATH = "maps/easy/01_linear_path.txt"
 
+
 def main():
     file_path = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PATH
-    print(file_path)
+    parser = Parser(file_path)
+    content = parser.load_raw_input()
+    print(content)
+
 
 if __name__ == "__main__":
     try:
         main()
-    except PersonalError:
-        print(PersonalError)
+    except PersonalError as e:
+        print(e)
         sys.exit(1)
--- src/parser.py
+++ src/parser.py
@@ -1,14 +1,15 @@
 class PersonalError(Exception):
     pass
 
-class parser:
+
+class Parser:
     def __init__(self, file_path):
         self.file_path = file_path
 
     def load_raw_input(self):
         try:
             with open(self.file_path, 'r') as file:
-                content = file.readlines()
-                return content
+                # read file object as file and return it as a list of newline
+                return file.read().splitlines()
         except (PermissionError, FileNotFoundError, IsADirectoryError) as e:
             raise PersonalError(f"file error: {e}")
