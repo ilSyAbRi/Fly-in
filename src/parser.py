092b205dcd782963f299ea1ff69d4d6113fd6c73
ilSyAbRi <iliasssyabri1@gmail.com>
2026-06-06 19:32:31 +0100

    start work on parse hub

--- src/parser.py
+++ src/parser.py
@@ -121,8 +121,23 @@ class Parser:
 
         return (k, v)
 
+    def parse_hubs(self, clean_indexed_ln):
+        
+        required_hub = ("hub", "start_hub", "end_hub")
+       
+        hubs = [x for x in clean_indexed_ln if x[1].startswith("hub")]
+        start_hub = [x for x in clean_indexed_ln if x[1].startswith("start_hub")]
+        end_hub = [x for x in clean_indexed_ln if x[1].startswith("end_hub")]
+        
+        if (len(start_hub) == 0):
+            raise CustomParserError("there are no start_hub it should be there")
+        
+        if (len(end_hub) == 0):
+           raise CustomParserError("there are no end_hub it should be there")
+         
     def dispatcher(self) -> None:
         clean_indexed_ln = self.load_raw_input()
         nb_drones_data = self.parse_nb_drones(clean_indexed_ln)
+        self.parse_hubs(clean_indexed_ln)
         print(clean_indexed_ln)
         print(nb_drones_data)
