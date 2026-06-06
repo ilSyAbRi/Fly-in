01be817b9c241199d6aa7757decb88edc42c01fe
ilSyAbRi <iliasssyabri1@gmail.com>
2026-06-06 15:51:46 +0100

    just make it clear

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 class Zone:
+
     def __init__(self, name: str, x: int, y: int,
                  zone_type: str = "normal",
                  color: str = "none",
                  max_drones: int = 1):
+
         self.name = name
         self.x = x
         self.y = y
