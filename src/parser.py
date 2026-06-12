5afa394b7d9aee40b4d14dfc4085d206146ec9e0
ilSyAbRi <iliasssyabri1@gmail.com>
2026-06-12 17:43:12 +0100

    start handle hub and check duplicate

--- src/parser.py
+++ src/parser.py
@@ -24,6 +24,7 @@ class LineCleaner:
         just set lines to clean to self.data to start cleaning
         """
         self.data = lines_to_clean
+        self.duplicate_list = []
 
     def remove_comments(self) -> "LineCleaner":
         """
@@ -46,7 +47,6 @@ class LineCleaner:
         self.data = [line for line in self.data if line]
         return self
 
-
 class Parser:
     """
     the main parser class
@@ -57,6 +57,7 @@ class Parser:
         set instance with attribute file_path
         """
         self.file_path = file_path
+        self.duplicate_list = []
 
     def load_raw_input(self) -> list[tuple]:
         """
@@ -74,6 +75,7 @@ class Parser:
                 raw_ln = LineCleaner(lines)
                 raw_ln.remove_comments().strip_spaces()
                 clean_ln = LineCleaner(lines)
+                # explain chaining
                 clean_ln.remove_comments().strip_spaces().remove_empty_lines()
                 index_lines = [
                         (i, v)
@@ -109,13 +111,24 @@ class Parser:
             raise StandardParserError(f"Line: {clean_indexed_lns[0][0]}"
                                       f"\nError : '{clean_indexed_lns[0][1]}'"
                                       " invalid syntax")
-        print(clean_indexed_lns)
         return nb
 
-    def parse_start_hub(self,nb_line, line: tuple):
+    def parse_hub(self,nb_line, line: tuple):
+
         try:
             start_hub, data = line.split(':')
             name, x, y, metadata = data.split()
+            x = int(x)
+            y = int(y)
+            for _n, _x, _y in self.duplicate_list:
+                if name == _n or (x == _x and y == _y):
+                    raise CustomParserError(f"Line: {nb_line}"
+                                            f"\nError: '{line}' "
+                                            "duplicate problem"
+                                            " check name x y")
+            # epxlain deference beteween append and extend
+            self.duplicate_list.append((name, x, y))
+
         except ValueError:
             raise StandardParserError(f"Line: {nb_line}"
                   f"\nError: '{line}'"
@@ -124,17 +137,18 @@ class Parser:
     def validate_extract_data(self, clean_indexed_lns: list[tuple]) -> None:
 
         max_drone = self.parse_nb_drones(clean_indexed_lns)
-
+        
+        # explain slicing
         for index, line in clean_indexed_lns[1:]:
             if line.startswith("nb_drones:"):
                 raise CustomParserError(f"Line: {index}"
-                                        "\nError: '{line}' duplicate")
+                                        f"\nError: '{line}' duplicate")
             elif line.startswith("start_hub:"):
-                self.parse_start_hub(index, line)
+                self.parse_hub(index, line)
             elif line.startswith("end_hub:"):
-                pass
+                self.parse_hub(index, line)
             elif line.startswith("hub:"):
-                pass
+                self.parse_hub(index, line)
             elif line.startswith("connection:"):
                 pass
             else:
