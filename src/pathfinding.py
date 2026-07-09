066d0b8a3943a07478ccff2dd608288c73d7f1ce
ilSyAbRi <iliasssyabri1@gmail.com>
2026-07-09 18:37:52 +0100

    do a big process in algo

--- src/pathfinding.py
+++ src/pathfinding.py
@@ -1,4 +1,6 @@
 
+import heapq
+
 class PathFinding():
     def __init__(self, graph):
         self.graph = graph
@@ -11,17 +13,20 @@ class PathFinding():
         visited = set()
         dist = {key: float("inf") for key in self.graph.adj.keys()}
         dist[self.start_node] = 0
-        current = self.start_node
-        com_cost = 0
-        for key, cost in dist.items():
+        pq = []
 
-            if dist[current] > dist[key]:
-                current = key
-        neighbors = self.graph.get_neighbors(current)
-        for neighbor in neighbors:
-            neighbor_zone, connection, cost = neighbor
+        while pq:
+            heapq.heappush(pq, (0, self.start_node))
+            current_cost, current_node = heapq.heappop(pq)
+            neighbors = self.graph.get_neighbors(current_node)
+            for neighbor in neighbors:
+                neighbor_node, connection, cost = neighbor
+                new_cost = dist[current_node] + cost
+                if new_cost < dist[neighbor_node.name]:
+                    dist[neighbor_node.name] = new_cost
+                    parent[neighbor_node.name] = current_node
+                    heapq.heappush(pq,(new_cost, neighbor_node))
 
-            new_cost = dist[current] + cost
 
     def dispatcher(self):
         self.djikstra(self.start_node, self.end_hub)
